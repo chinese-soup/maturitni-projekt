49cb8b8d7ad42920ab74d1f8b0d7beb1f35cf4b0
Jakub Mach <jakub.m4ch@gmail.com>
2016-04-18 03:31:26 +0200

    docs

--- docs/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['references_20for_20web_2dclient_2fapi_2f',['References for web-client/api/',['../md__home_unko_code_maturitni-projekt_web-client_api__r_e_f.html',1,'']]]
-];
--- docs/html/search/all_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['web_2dclient',['Web-client',['../md__home_unko_code_maturitni-projekt_web-client__r_e_a_d_m_e.html',1,'']]],
-  ['web_2dclient_2fstatic',['Web-client/static',['../md__home_unko_code_maturitni-projekt_web-client_static__r_e_a_d_m_e.html',1,'']]]
-];
